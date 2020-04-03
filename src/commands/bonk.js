788b5d3f77b6a7ee276317df4d92063700acdfcc
Douglas Marcelino <douglas_champion@hotmail.com>
2020-04-02 23:32:19 -0300

    Ajusta função bonk (quando não menciona user)

--- src/commands/bonk.js
+++ src/commands/bonk.js
@@ -6,13 +6,19 @@ module.exports = {
         
         if(cmd === `bonk`){
 
+            
+                
+
             let attachment;
             const effectsBonk = ["original", "dreamy", "ripples", "rainbow"];
             const randomEffect = effectsBonk[Math.floor(Math.random() * effectsBonk.length)];
 
             attachment = bonkStrategy[randomEffect]();
 
-            await message.channel.send(`${message.author} bonkou ${args[0]}`, attachment);
+            if(args[0])
+                await message.channel.send(`${message.author} bonkou ${args[0]}`, attachment);
+            else
+                await message.channel.send(`Você se bonkou, ${message.author}`, attachment);
         }
     }
 }

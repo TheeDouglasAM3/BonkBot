b056f418d6497ef016184f1e5e1b02e12854e160
Douglas Marcelino <douglas_champion@hotmail.com>
2020-04-04 23:15:00 -0300

    Atualiza comando bonk.js

--- src/commands/bonk.js
+++ src/commands/bonk.js
@@ -2,23 +2,52 @@ const bonkStrategy = require('../classes/bonkStrategy');
 
 module.exports = { 
 
-    async bonk(cmd, message, args){
+    async bonk(cmd, message, args, ownIdBot){
         
         if(cmd === `bonk`){
 
-            
-                
-
             let attachment;
-            const effectsBonk = ["original", "dreamy", "ripples", "rainbow"];
-            const randomEffect = effectsBonk[Math.floor(Math.random() * effectsBonk.length)];
+            const idMembroMencionado = this._algumMembroFoiMencionado(message)
+                ? message.mentions.users.first().id
+                : null
+            
+            if (idMembroMencionado == ownIdBot){
 
-            attachment = bonkStrategy[randomEffect]();
+                attachment = bonkStrategy["bot"]();
 
-            if(args[0])
-                await message.channel.send(`${message.author} bonkou ${args[0]}`, attachment);
-            else
-                await message.channel.send(`Você se bonkou, ${message.author}`, attachment);
+                try {
+                    await message.channel.send(`Você não pode me bonkar. O BonkBot bonkou você, ${message.author}`, attachment);
+                } catch (error) {
+                    console.log(error);
+                }
+                
+            }else{
+                
+                const effectsBonk = ["original", "dreamy", "ripples", "rainbow", "glitch", 
+                "onFire", "party"];
+
+                const randomEffect = effectsBonk[Math.floor(Math.random() * effectsBonk.length)];
+
+                attachment = bonkStrategy[randomEffect]();
+
+                try {
+                    if(this._membroMencionadoNaoEhOProprioUsuario(message, idMembroMencionado))
+                        await message.channel.send(`${message.author} bonkou ${args[0]}`, attachment);
+                    else
+                        await message.channel.send(`Você se bonkou, ${message.author}`, attachment);
+                } catch(error) {
+                    console.log(error);
+                }
+
+            }    
         }
+    },
+
+    _algumMembroFoiMencionado(message){
+        return message.mentions.users.first() != null;
+    },
+
+    _membroMencionadoNaoEhOProprioUsuario(message, idMembroMencionado){
+        return idMembroMencionado != null && idMembroMencionado !== message.author.id;
     }
 }

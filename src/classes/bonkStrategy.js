b1880b3914c0a70b75ab5c4d62d0ea001dbad620
Douglas Marcelino <douglas_champion@hotmail.com>
2020-04-04 23:14:43 -0300

    Atualiza bonkStrategy (acrescenta mais conteúdo)

--- src/classes/bonkStrategy.js
+++ src/classes/bonkStrategy.js
@@ -1,23 +1,20 @@
 const { MessageAttachment } = require("discord.js");
 
-function originalBonk(){
+function botBonk() {return new MessageAttachment('img/assets/bonkBot.gif')}
 
-    return new MessageAttachment('img/assets/bonk.gif')
-}
-function dreamyBonk(){
+function originalBonk() {return new MessageAttachment('img/assets/bonk.gif')}
+function dreamyBonk() {return new MessageAttachment('img/assets/bonkDreamy.gif')}
+function ripplesBonk() {return new MessageAttachment('img/assets/bonkRipples.gif')}
+function rainbowBonk() {return new MessageAttachment('img/assets/bonkRainbow.gif')}
+function glitchBonk() {return new MessageAttachment('img/assets/bonkGlitch.gif')}
+function onFireBonk() {return new MessageAttachment('img/assets/bonkOnFire.gif')}
+function partyBonk() {return new MessageAttachment('img/assets/bonkParty.gif')}
 
-    return new MessageAttachment('img/assets/bonkDreamy.gif')
-}
-function ripplesBonk(){
-
-    return new MessageAttachment('img/assets/bonkRipples.gif')
-}
-function rainbowBonk(){
-
-    return new MessageAttachment('img/assets/bonkRainbow.gif')
-}
-
+module.exports["bot"] = botBonk;
 module.exports["original"] = originalBonk;
 module.exports["dreamy"] = dreamyBonk;
 module.exports["ripples"] = ripplesBonk;
 module.exports["rainbow"] = rainbowBonk;
+module.exports["glitch"] = glitchBonk;
+module.exports["onFire"] = onFireBonk;
+module.exports["party"] = partyBonk;
